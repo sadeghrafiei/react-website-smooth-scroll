85ebc4b37a1d7a1aea6839dc09bb4e6510d85f29
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-01 23:42:08 +0330

    add navMenu , navLinks,NavItems

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Link as LinkR } from "react-router-dom";
-// import { Link as LinkS} from 'react-scroll';
+import { Link as LinkS} from 'react-scroll';
 
 export const Nav = styled.nav`
   background: #000;
@@ -56,3 +56,32 @@ export const MobileIcon = styled.div`
     }
 `
 
+export const NavMenu = styled.ul `
+    display: flex;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+    margin-right: -22px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`
+
+export const NavItem = styled.li`
+    height: 80px;
+`
+
+export const NavLinks = styled(LinkS)`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+
+    &.active {
+        border-bottom: 3px solid #01bf71;
+    }
+`
