c6af283a32bf20ed08c162d9aa3a99c42ed678ff
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-02 01:07:48 +0330

    add sidebarContainer,Icon,CloseIcon

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CloseIcon, Icon, SidebarContainer } from './SlidebarElements'
+
+const Sidebar = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
