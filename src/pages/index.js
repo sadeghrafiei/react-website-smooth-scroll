08d9a9674422f0c2dd3d842e249942e237de9dd3
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-02 16:31:44 +0330

    add heroSection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Hero from '../components/HeroSection'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 
@@ -13,7 +14,8 @@ const Home = () => {
     return (
         <>
          <Sidebar isOpen={isOpen} toggle={toggle} />
-         <Navbar toggle={toggle} />   
+         <Navbar toggle={toggle} /> 
+         <Hero />  
         </>
     )
 }
