87c4ac3cc94180bc1972a15769a6280fa0079cda
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-02 15:15:03 +0330

    add sidebar elements

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import { CloseIcon, Icon, SidebarContainer } from './SlidebarElements'
+import React from "react";
+import { CloseIcon, Icon, SidebarContainer, SidebarLink, SidebarMenu, SidebarRoute, SidebarWrapper, SideBtnWrap } from "./SlidebarElements";
 
 const Sidebar = () => {
-    return (
-        <SidebarContainer>
-            <Icon>
-                <CloseIcon />
-            </Icon>
-        </SidebarContainer>
-    )
-}
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="discover">Discover</SidebarLink>
+          <SidebarLink to="services">Services</SidebarLink>
+          <SidebarLink to="signup">Sign Up</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+            <SidebarRoute to="/signin">
+                Sign In
+            </SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
