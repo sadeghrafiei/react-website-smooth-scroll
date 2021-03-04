3f21ab5bce1b1752397e10d98a2779d74dcab64c
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-05 01:30:58 +0330

    add serviecs

---
+++ src/components/infoSection/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Button } from "../ButtonElements";
+import {
+  BtnWrap,
+  Column1,
+  Column2,
+  Heading,
+  Img,
+  ImgWrap,
+  InfoContainer,
+  InfoRow,
+  InfoWrapper,
+  Subtitle,
+  TextWrapper,
+  TopLine,
+} from "./InfoElements";
+
+
+const InfoSection = ({
+  img,
+  id,
+  lightBg,
+  lightText,
+  lightTextDesc,
+  topLine,
+  headline,
+  description,
+  buttonLabel,
+  imgStart,
+  alt,
+  dark,
+  primary,
+  darkText,
+  dark2,
+}) => {
+  return (
+    <>
+      <InfoContainer lightBg={lightBg} id={id}>
+        <InfoWrapper>
+          <InfoRow imgStart={imgStart}>
+            <Column1>
+              <TextWrapper>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
+                <BtnWrap>
+                  <Button
+                    to="home"
+                    primary={primary ? 1 : 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+                    smooth={true}
+                    duration={500}
+                    offset={-80}
+                    exact="true"
+                    spy={true}
+                  >
+                    {buttonLabel}
+                  </Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={img} alt={alt} />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
+};
+
+export default InfoSection;
