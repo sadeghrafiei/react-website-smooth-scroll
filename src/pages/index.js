be469c96318715328a33a6e18632e35a1869ff4d
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-05 01:24:13 +0330

    add services

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react'
-import Hero from '../components/HeroSection'
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
+import React, { useState } from "react";
+import Hero from "../components/HeroSection";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import InfoSection from "../components/infoSection";
+import {
+  homeObjFive,
+  homeObjFour,
+  homeObjOne,
+  homeObjSix,
+  homeObjThree,
+  homeObjTwo,
+} from "../components/infoSection/Data";
+import Services from "../components/Services";
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen , setIsOpen] = useState(false)
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const toggle = () => {
-        setIsOpen(!isOpen)
-    }
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <Hero />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <Services />
+      <InfoSection {...homeObjThree} />
+    </>
+  );
+};
 
-    return (
-        <>
-         <Sidebar isOpen={isOpen} toggle={toggle} />
-         <Navbar toggle={toggle} /> 
-         <Hero />  
-        </>
-    )
-}
-
-export default Home
+export default Home;
